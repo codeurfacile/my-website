b55a76452f2ae31161943da3fdf2d5d2320596a8
codeurfacile <124600249+codeurfacile@users.noreply.github.com>
2023-02-16 17:31:12 +0000

    Deploying to gh-pages from @ codeurfacile/my-website@ecd6a8d5dd6c254461740891db46bc9d61af66fc 🚀

--- my-website-6bf6d0e02ccac339.js
+++ my-website-6bf6d0e02ccac339.js
@@ -826,7 +826,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('my-website-56ba7ec966ca5bf4_bg.wasm', import.meta.url);
+        input = new URL('my-website-6bf6d0e02ccac339_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
